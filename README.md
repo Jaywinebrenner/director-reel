53101aba462f63e4101e9a46e42be3fa9f48e7aa
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-03-13 08:44:42 -0700

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-<ResponsivePlayer
-  url='https://virtual-counselor-video.s3-us-west-2.amazonaws.com/1.intro.2.mp4'
-
-  />
---
+++ precache-manifest.3cd6cd330e35cba88a5c2f45613055e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44aa93dc209f8cf499ca306d973ebf91",
+    "url": "/director-reel/index.html"
+  },
+  {
+    "revision": "2b5002b47ab77ff81fad",
+    "url": "/director-reel/static/css/main.7efc4a0a.chunk.css"
+  },
+  {
+    "revision": "2b3b25d9f78b01faa47f",
+    "url": "/director-reel/static/js/2.c3282393.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/director-reel/static/js/2.c3282393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5002b47ab77ff81fad",
+    "url": "/director-reel/static/js/main.796481ef.chunk.js"
+  },
+  {
+    "revision": "7aeb819b01fcbe3001bc",
+    "url": "/director-reel/static/js/runtime-main.f3792534.js"
+  }
+]);
