2f88171d1d12c425e766151256227fa4286ce3ce
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-03-13 08:30:06 -0700

    add home componenent

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import './App.css';
 import ResponsivePlayer from './ResponsivePlayer';
-
-
+import { Route } from 'react-router-dom';
+import Home from './home.jsx';
 
 function App() {
 
 
   return (
-    <div className="App">
-
-      <h1>Jay Winebrenner</h1>
-      <h3>Director Reel</h3>
-      <hr></hr>
-      <ResponsivePlayer
-        url='https://virtual-counselor-video.s3-us-west-2.amazonaws.com/Jay+Winebrenner+Reel.mp4'
-        loop='true'
-  />
-
-    <hr></hr>
-<h5>jaywinebrenner@gmail.com | <a style={{ color: '#524f4a', textDecoration: 'none'}}  href="https://www.linkedin.com/in/jaywinebrenner/">LinkedIn</a> </h5>
-
-
-    </div>
+        <Route path="/" component={ Home } />
   );
 }
 
